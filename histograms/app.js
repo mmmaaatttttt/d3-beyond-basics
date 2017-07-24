85f9b6c34fe8d0978bb60fb7518886f018a88d0a
Matt Lane <lane.matthew@gmail.com>
2017-07-23 22:17:52 -0700

    grabbing data and formatting it with d3.csv

--- histograms/app.js
+++ histograms/app.js
@@ -1,7 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  d3.json('https://data.sfgov.org/resource/sipz-fjte.json', function(data) {
-    console.log(data);
+  d3.csv('../data/Restaurant_Scores_-_LIVES_Standard.csv', function(d, i, x, y) {
+    return {
+      id: d.business_id,
+      address: d.business_address,
+      lat: d.business_latitude,
+      long: d.business_longitude,
+      zip: d.business_postal_code,
+      inspection: {
+        id: d.inspection_id,
+        date: new Date(d.inspection_date),
+        score: +d.inspection_score,
+        type: d.inspection_type,
+        violations: [{
+          id: d.violation_id,
+          description: d.violation_description,
+          riskCategory: d.risk_category
+        }]
+      }
+    }
+  }, function(data) {
+
+    // group inspections together
+    data = data.reduce(function(acc, next, i) {
+      var last = acc[acc.length - 1];
+      if (last && next.inspection.id === last.inspection.id) {
+        last.inspection.violations.push(next.inspection.violations[0])
+      } else {
+        acc.push(next);
+      }
+      return acc;
+    }, []);
+
+    // d3.histogram
+    debugger;
+    
   });
 
 });
